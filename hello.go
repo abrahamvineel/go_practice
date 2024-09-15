c8aec6dc436fda01ee3a279b6d537e77aefdafaa
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-14 21:07:57 -0400

    conversions in go

--- hello.go
+++ hello.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
+// func main() {
+// 	fmt.Println("hello world")
 
-func main() {
-	fmt.Println("hello world")
+// 	i := 1
+// 	for i <= 3 {
+// 		fmt.Println(i)
+// 		i = i + 1
+// 	}
 
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
-	}
-
-	if num := 9; num < 0 {
-		fmt.Println("negative number")
-	} else if num < 10 {
-		fmt.Println(num, " has 1 digit")
-	}
-}
+// 	if num := 9; num < 0 {
+// 		fmt.Println("negative number")
+// 	} else if num < 10 {
+// 		fmt.Println(num, " has 1 digit")
+// 	}
+// }
