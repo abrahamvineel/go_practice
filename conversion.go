265e8f83663f0700d3e14c7beaae7806ea2d608f
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-16 20:38:17 -0400

    pointers and slices

--- conversion.go
+++ conversion.go
@@ -1,24 +1,16 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+// func main() {
+// 	fmt.Println("Please rate out pizza ")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	input, _ := reader.ReadString('\n')
+// 	fmt.Println("thanks for rating, ", input)
 
-func main() {
-	fmt.Println("Please rate out pizza ")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	fmt.Println("thanks for rating, ", input)
+// 	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
 
-	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("rating is increased to,", numRating+1)
-	}
-}
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		fmt.Println("rating is increased to,", numRating+1)
+// 	}
+// }
--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-// func main() {
-// 	fileServer := http.FileServer(http.Dir("./static"))
-// 	http.Handle("/", fileServer)
-// 	http.HandleFunc("/form", formHandler)
+import (
+	"fmt"
+)
 
-// 	fmt.Printf("Starting server at port 8080\n")
-// 	if err := http.ListenAndServe(":8080", nil); err != nil {
-// 		log.Fatal(err)
-// 	}
+func main() {
+	// var ptr *int
+	// fmt.Println("value of pointer is ", ptr)
 
-// }
+	myNumber := 23
+	var ptr = &myNumber
+
+	// fmt.Println("value of actual pointer is", ptr)
+	// fmt.Println("value of actual pointer is", *ptr)
+
+	*ptr = *ptr * 2
+	// fmt.Println("new value of actual pointer is", myNumber)
+
+	//slices
+
+	var fruitList = []string{"Apple"}
+	fmt.Printf("type of fruit list %T\n", fruitList)
+
+	fruitList = append(fruitList, "banana")
+
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:])
+	fmt.Println(fruitList)
+}
