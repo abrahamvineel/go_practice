04c4ec645637fc8a0ebcdae6f043159a735f58b3
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-10-20 20:44:04 -0400

    for

--- main.go
+++ main.go
@@ -11,37 +11,7 @@ func main() {
 	// var ptr *int
 	// fmt.Println("value of pointer is ", ptr)
 
-	myNumber := 23
-	var ptr = &myNumber
-
-	// fmt.Println("value of actual pointer is", ptr)
-	// fmt.Println("value of actual pointer is", *ptr)
-
-	*ptr = *ptr * 2
-	// fmt.Println("new value of actual pointer is", myNumber)
-
-	//slices
-
-	var fruitList = []string{"Apple"}
-	fmt.Printf("type of fruit list %T\n", fruitList)
-
-	fruitList = append(fruitList, "banana")
-
-	fmt.Println(fruitList)
-
-	fruitList = append(fruitList[1:])
-	fmt.Println(fruitList)
-
-	//no inheritance in golang; no super or parent
-
-	abraham := User{"Abraham", "abc.xyz.com", true, 16}
-	fmt.Printf("%+v\n", abraham)
-	fmt.Printf("Name is %v and email is %v\n", abraham.Name, abraham.Email)
-}
-
-type User struct {
-	Name   string
-	Email  string
-	Status bool
-	Age    int
+	for i := range 6 {
+		fmt.Println(i)
+	}
 }
